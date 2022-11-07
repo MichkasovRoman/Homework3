db2288b0a90252b95149435760661329e7ddbeee
Michkasov Roman <mihjob@list.ru>
2022-11-07 23:18:45 +0300

    Wrote the text of the code27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая
+//принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum_of_Digits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum = sum + n%10;
+        n = n/10;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр введенного вами числа равна {Sum_of_Digits(number)}");
