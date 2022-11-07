e7e71f6e254b8d6827dfd103011bc2f60005107c
Michkasov Roman <mihjob@list.ru>
2022-11-07 22:59:03 +0300

    Wrote the text of the code

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B.
+
+int A_to_Power_of_B (int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i ++)
+    {
+        result = result*A;
+    }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число A = ");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B = ");
+int numb2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Результат возведения {numb1} в степень числа {numb2} равен {A_to_Power_of_B(numb1, numb2)}");
