5ce287740d9c4d901a315a26fb22cc9ea4454f3b
Michkasov Roman <mihjob@list.ru>
2022-11-08 01:15:30 +0300

    Wrote the text of the code_add

---
+++ Additional_task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача “со звездочкой”: Напишите функцию, которая
+//принимает одно число - высоту елочки и рисует ее в
+//консоли звездочками.
+
+string Spruce(int n)
+{
+    string star = "*";
+    for (int i = 1; i < n; i++)
+    {
+        Console.WriteLine(" " + star);
+        star = "*" + star + "*";
+    }
+    return star;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Spruce(number));
