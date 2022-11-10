75045c32659439dc00fa020eeb24314c350b3dc9
Michkasov Roman <mihjob@list.ru>
2022-11-11 00:43:59 +0300

    changed the text of the code Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,14 +2,23 @@
 //два числа (A и B) и возводит число A в натуральную
 //степень B.
 
-int A_to_Power_of_B (int A, int B)
+void A_to_Power_of_B (int A, int B)
 {
-    int result = 1;
-    for (int i = 1; i <= B; i ++)
+    if (B > 0)
     {
-        result = result*A;
+        int result = 1;
+        for (int i = 1; i <= B; i ++)
+        {
+          result = result*A;  
+        }
+        Console.WriteLine($"Результат возведения {A} в степень числа {B} равен {result}");
+    } 
+    
+    else
+    {
+        Console.WriteLine($"Число {B} не является натуральным.");
+        Console.WriteLine("Перезапустите программу и введите числа еще раз.");
     }
-    return result;
 }
 
 Console.Clear();
@@ -17,4 +26,4 @@ Console.Write("Введите число A = ");
 int numb1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число B = ");
 int numb2 = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Результат возведения {numb1} в степень числа {numb2} равен {A_to_Power_of_B(numb1, numb2)}");
+A_to_Power_of_B(numb1, numb2);
